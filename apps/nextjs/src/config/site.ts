9a6a50125b56e6db87ce671d4054643aa15e5414
bshongwe <dru.da.beat.hustle@gmail.com>
2024-09-09 20:28:26 +0200

    Revert to other pre-template I had

--- apps/nextjs/src/config/site.ts
+++
@@ -1,9 +0,0 @@
-export const siteConfig = {
-  name: "Bunny-Ai",
-  description: "Your Ai buddy",
-  url: "https://github.com/bshongwe/headstarter-capstone-project_waiting-list",
-  ogImage: "",
-  links: {
-    github: "https://github.com/saasfly/saasfly",
-  },
-};
