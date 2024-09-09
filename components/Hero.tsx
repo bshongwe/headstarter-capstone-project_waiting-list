e4e8eb8c724d2f0d9ee8b5e8f2a789c3c4e6b14b
bshongwe <dru.da.beat.hustle@gmail.com>
2024-09-09 22:22:04 +0200

    Update hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,14 +12,14 @@ const Hero = () => {
                 <main className="text-5xl md:text-6xl font-bold">
                     <h1 className="inline">
                         <span className="inline bg-gradient-to-r from-[#F596D3] to-[#D247BF] text-transparent bg-clip-text">
-                            Bunny
+                            Bunny-AI
                         </span>{" "}
                         -
                     </h1>{" "}
-                    A{" "}
+                    more than just an{" "}
                     <h2 className="inline">
                         <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-                            I
+                            AI
                         </span>{" "}
                         buddie.
                     </h2>
